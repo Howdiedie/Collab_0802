12b18dd177c8efdc5c446e6e141df79365f7df59
Chi <qqq011334466@gmail.com>
2023-07-17 16:43:28 +0800

    加入專案檔案。

---
+++ Collab/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace Collab.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
