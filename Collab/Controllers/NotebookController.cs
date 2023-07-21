9b215edef641958aa1b68efa0d52e3ff5dc80dbd
Howdiedie <kent81001@gmail.com>
2023-07-21 11:14:34 +0800

    0721測試push

--- Collab/Controllers/NotebookController.cs
+++ Collab/Controllers/NotebookController.cs
@@ -2,6 +2,8 @@
 
 namespace collab_00.Controllers {
     public class NotebookController : Controller {
+
+        //45
         public IActionResult Index() {
             return View();
         }
