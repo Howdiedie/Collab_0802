8d3b567edd9d1810a81ce07078bce85765f70326
hcw30790 <jenny81252000@gmail.com>
2023-07-21 15:37:44 +0800

    Sign跳轉視窗優化 資料可以匯進資料庫

--- Collab/Controllers/MissionEditController.cs
+++ Collab/Controllers/MissionEditController.cs
@@ -6,16 +6,7 @@ namespace collab_00.Controllers {
 
         public IActionResult Index() {
 
-            //// 從資料庫獲取數據
-            //List<string> IntentName = new List<string>();
-            //var intents = _dbContext.Intents;
-            //foreach (var intent in intents)
-            //{
-            //    IntentName.Add(intent.Name);
-            //}
-
-            //// 將目標傳遞給視窗
-            //ViewBag.IntentName = IntentName;
+            
             return View();
         }
     }
