c884769892f5808ac0dcd509f3235d7da8d69ea1
Ruei <aa821026@gmail.com>
2023-07-21 17:29:00 +0800

    1.確認Email格式  2.確認是否有該帳戶(第一欄)

--- Collab/Controllers/ForgetPWController.cs
+++ Collab/Controllers/ForgetPWController.cs
@@ -1,9 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Azure;
+using Collab.Models;
+using Microsoft.AspNetCore.Mvc;
 
-namespace collab_00.Controllers {
-    public class ForgetPWController : Controller {
-        public IActionResult Index() {
+namespace collab_00.Controllers
+{
+    public class ForgetPWController : Controller
+    {
+        private readonly TestBananaContext _TestBananaContext;
+        public ForgetPWController(TestBananaContext context)
+        {
+            _TestBananaContext = context;
+        }
+        public IActionResult Index()
+        {
             return View();
         }
+        public ActionResult MemberEmail(string memberaccount)
+        {
+            var user = _TestBananaContext.Members.FirstOrDefault(m => m.MemberAccount == memberaccount);
+            if (user != null)
+            {
+                //SendMailTokenOut outModel = new SendMailTokenOut();
+
+
+
+
+                ViewBag.memberaccount=memberaccount;
+                return View("Index"); 
+            }
+            else {
+                ViewBag.notfound = "未搜尋到帳戶，請確認後再輸入";
+                return View("Index");
+            }
+        }
     }
+
 }
